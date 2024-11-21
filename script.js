6e7b552d428d417a83ed88141c86c991f4e1e7d3
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-21 16:21:36 +0530

    CLASS AND OBJECT

---
+++ script.js
@@ -0,0 +1,18 @@
+class Book{
+    title;
+    author;
+    isbn;
+    constructor(bookTitle,bookAuthor,bookIsbn){
+        this.title = bookTitle;
+        this.author = bookAuthor;
+        this.isbn = bookIsbn;
+    }
+}
+class MembershipType{
+    name;
+    membershiptype;
+    constructor(memberName,memberShipType){
+        this.memberName = memberName;
+        this.membershiptype = memberShipType;
+    }
+}
