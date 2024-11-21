a8aab466ed50c02c3e6b05871c03039291989397
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-21 16:32:41 +0530

    CLASS AND OBJECT

--- script.js
+++ script.js
@@ -7,12 +7,31 @@ class Book{
         this.author = bookAuthor;
         this.isbn = bookIsbn;
     }
+    checkOut(){
+
+    }
+    returnBook(){
+
+    }
 }
 class MembershipType{
     name;
     membershiptype;
     constructor(memberName,memberShipType){
-        this.memberName = memberName;
+        this.name = memberName;
         this.membershiptype = memberShipType;
     }
+    borrowBook(book){
+        console.log(`This ${book} is borrowed by ${this.name}`);
+
+    }
+    returnBook(book){
+        console.log(`This ${book} is returned by ${this.name}`);
+    }
 }
+
+var book1 = new Book("Javascript", "Oracle", "2373926");
+console.log(book1);
+var member = new MembershipType("Pavan Kalyan", "Premium");
+member.borrowBook(book1.title);
+member.returnBook("book2");
